49adfa6098fe531f50c782aaf1a6f525e441b299
anandrajkcet09 <anandraj.kcet@gmail.com>
2026-01-22 21:59:12 +0530

    Update code and views

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listings.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -24,6 +25,9 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
+
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
@@ -31,8 +35,8 @@ app.get("/", (req, res) => {
 
 //Index Route
 app.get("/listings", async (req, res) => {
-    const allListing = await Listing.find({});
-    res.render("./listings/index.ejs", {allListing});
+    const allListings = await Listing.find({});
+    res.render("./listings/index.ejs", {allListings});
 });
 
 //New Route
--- models/listings.js
+++ models/listings.js
@@ -3,22 +3,21 @@ const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
     title: {
-        type: String, 
+        type: String,
         required: true
     },
-    description:String,
-    image:{
+    description: String,
+    image: {
         filename: String,
         url: {
             type: String,
-        default:"https://unsplash.com/photos/cozy-living-room-with-sectional-sofa-and-coffee-table-Ie3fp14d_NQ",
-
-        set: (v) => v==="" ? "https://unsplash.com/photos/cozy-living-room-with-sectional-sofa-and-coffee-table-Ie3fp14d_NQ" : v,
+            default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+            set: (v) => v === "" ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" : v,
+        },
     },
-},
-    price:Number,
-    location:String,
-    country:String,
+    price: Number,
+    location: String,
+    country: String,
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
